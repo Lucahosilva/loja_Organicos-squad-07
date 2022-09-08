cfdf58d966bbed0ef5bc1759e8f49401fc3cf015
Gabriel <gabriel.tiui@hotmail.com>
2022-09-08 17:08:23 -0300

    gabriel

--- organicos.py
+++ organicos.py
@@ -25,3 +25,62 @@ print(f'Bem vindo {user}!!!\n\n{menu}')
 
 #Chamar outras funções do menu 2
 opcoes = 0
+
+
+# your_name=input("Qual é o seu nome? ")
+#print("\n Prazer em lhe conhecer {}".format(your_name.capitalize()))
+
+
+def menu_funcs():
+	print("""
+		Essas são minhas funções:
+			(1) = Cadastro
+			(2) = Vendas
+			(3) = Relatório
+			(0) = Para SAIR
+
+	""")
+	
+menu_funcs()
+
+
+first_choice= input ("Qual a função desejada? ")
+
+if first_choice == "0":
+	print("{}, agradeço sua companhia e espero que tenha ajudado.".format(your_name.capitalize()))
+	exit()
+
+elif first_choice == "1":
+
+	def menu_organics():
+		
+		print("""
+
+        lacunas pra preencher
+        Cadastro
+
+			
+		""")
+	menu_organics()
+
+elif first_choice == "2":
+
+	def menu_vendas():
+		
+		print("""
+
+        lacunas pra preencher
+        Vendas
+			
+		""")
+	menu_vendas()
+elif first_choice == "3":
+    def relatorios():
+
+        print("""
+        
+        Lacunas a preencher
+		Relatório
+        
+        """)
+    relatorios()
