b4f7de71db32d05c0db0d5649f3516480e367e19
Lucas Silva <lucas.henrique@luizalabs.com>
2022-09-08 19:41:44 -0300

    Subindo a segunda versão do carrinho de compras

--- vendas.py
+++ vendas.py
@@ -1,22 +1,31 @@
 carrinho = []
+valorCarrinho = []
 valorTotal = []
 lista_produtos = {"graos" : 10.00 , "feijão": 20 , "arroz" : 45.50}
 final = "S" #joga o programa para dentro do laço do carrinho
 
 while final != "N":
-    print(f'''
-    Produtos disponiveis: 
-    {lista_produtos}
-    ''')
+    print(f"Produtos disponiveis: \n{lista_produtos}" , "\n")
     itemadd = (input("por favor digite o nome do produto a ser adicionado ?"))
-    carrinho.append(lista_produtos[itemadd])
-    print ("carrinho atualizado: " , carrinho)
 
+    while not itemadd in lista_produtos: #verifica se o produto está cadastrado.
+        print("produto não cadastrado, por favor digite outro")
+        itemadd = (input("por favor digite o nome do produto a ser adicionado ?"))
+
+    carrinho.append(itemadd)
+    valorCarrinho.append(lista_produtos[itemadd])
+    print ("carrinho atualizado: \n" , carrinho , )
+    
     final = str(input("deseja adicionar mais intens? (S/N)")).upper()
 
 
-valorTotal = sum(carrinho)
-print("o valor final é " , valorTotal)
+valorTotal = sum(valorCarrinho)
+print(f'''
+Resumo: 
+{carrinho}
+Valor total é: {valorTotal}
+
+''')
 
 
 
