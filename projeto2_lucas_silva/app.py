92549e6c671977cb77ece7857e0701ffd4b14b32
lucas henrique <lucashosilva@gmail.com>
2022-10-10 18:26:02 -0300

    adicionado rota de deletar WIP

--- projeto2_lucas_silva/app.py
+++ projeto2_lucas_silva/app.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 app = Flask(__name__)
-catalogo = pd.read_csv('projeto2_lucas_silva/catalogo.csv', sep=';')
+catalogo = pd.read_csv('projeto2_lucas_silva/catalogo.csv', sep=',')
 cart = pd.DataFrame({"Produto":[],"valor":[], "Quantidade":[]})
 total = 0
 
@@ -15,16 +15,23 @@ def carrinho(pag):
     catalogo_pag = catalogo.iloc[6*(pag-1):6*pag]
     return render_template('carrinho.html', catalogo = catalogo_pag, pag=pag)
 
-@app.route('/produto_adicionado/<produto>/<valor>')
-def teste(produto , valor):
-        cart.loc[len(cart)] = produto , valor, 1 
+@app.route('/produto_adicionado/<produtos>/<valor>')
+def teste(produtos , valor):
+        cart.loc[len(cart)] = produtos , valor, 1 
         cart.to_csv('projeto2_lucas_silva/cart.csv')       
         return redirect ('/carrinho/1')
+
+@app.route('/produto_excluido/<produtos>')
+def excluir(produtos):
+    cart.drop(produtos, inplace = True)
+    cart.to_csv('projeto2_lucas_silva/cart.csv')  
+    return redirect('Checkout.html', produtos = produtos)
     
 @app.route('/finalizar')
 def finalizar():
-    total = 10
-    return render_template('Checkout.html' ,cart =cart)
+    #cart['valor']=float(cart['valor'])
+    total= cart['valor']
+    return render_template('Checkout.html' ,cart =cart, total = total)
     
 if __name__ == "__main__":
     app.run(debug=True)
