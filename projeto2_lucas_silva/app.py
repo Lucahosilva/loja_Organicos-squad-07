a1bf330a240747a9e86ed4db78658ade20eb4350
Lucas Henrique <lucashosilva@gmail.com>
2022-10-13 18:06:28 -0300

    Commitando a nova alteração de histórico

--- projeto2_lucas_silva/app.py
+++ projeto2_lucas_silva/app.py
@@ -1,13 +1,11 @@
-
+import os
 from flask import Flask, url_for, redirect, render_template, request
 import pandas as pd
-
-
+from datetime import datetime
 
 app = Flask(__name__)
-catalogo = pd.read_csv('catalogo.csv', sep=',')
-cart = pd.DataFrame({"Produto":[],"valor":[], "Quantidade":[], "total":[]})
-cart.set_index('Produto', inplace = True)
+catalogo = pd.read_csv('projeto2_lucas_silva/catalogo.csv', sep=',')
+cart = pd.read_csv('projeto2_lucas_silva/cart.csv', sep=',', index_col='Produto')
 total = 0
 
 @app.route('/carrinho/<pag>')
@@ -22,17 +20,15 @@ def teste(produtos , valor):
         cart.loc[produtos,"Quantidade"] = float(cart.loc[produtos,"Quantidade"]) + 1 
     else:
         cart.loc[produtos] =  [valor, 1, 0]
-    cart.to_csv('cart.csv')       
+    cart.to_csv('projeto2_lucas_silva/cart.csv')       
     return redirect(request.referrer)
-    #return redirect ('/carrinho/1')
 
 @app.route('/produto_excluido/<produtos>')
 def excluir(produtos):
     cart.drop(produtos, inplace = True)
-    cart.to_csv('cart.csv')  
+    cart.to_csv('projeto2_lucas_silva/cart.csv')  
     return redirect('/finalizar')
-    
-    
+  
 @app.route('/finalizar')
 def finalizar():
     argumentos = request.args.to_dict()
@@ -48,6 +44,17 @@ def finalizar():
     total_produto= (cart['Quantidade'].astype(float)*cart['valor'].astype(float))
     total= cart["total"].astype(float).sum()
     return render_template('Checkout.html' ,cart =cart, total = total)
+
+@app.route('/obrigado')
+def historico():
+    df_historico = pd.read_csv('projeto2_lucas_silva/cart.csv', sep=',', index_col='Produto')
+    df_historico['data']=datetime.today()
+    df_sales =pd.read_csv('projeto2_lucas_silva/sales.csv',  sep=',', index_col='Produto')
+    result = [df_historico, df_sales]
+    df_result =pd.concat(result)
+    df_result.to_csv('projeto2_lucas_silva/sales.csv')
+       
+    return 'deu certo'
     
 if __name__ == "__main__":
     app.run(debug=True)
